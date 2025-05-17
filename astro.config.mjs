3cde848db2b39dc5acb6deae34166761046a9eee
Ikrlegido12 <Ikermardi2010huawei@gmail.com>
2025-05-17 22:43:40 +0200

    empezar de nuevo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,10 @@
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:2587226798.
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-import clerk from "@clerk/astro";
-
-import tailwindcss from '@tailwindcss/vite';
-
-import node from '@astrojs/node';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
-  integrations: [react(), clerk()],
-
-  output: "server",
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
