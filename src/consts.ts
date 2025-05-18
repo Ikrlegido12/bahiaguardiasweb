366ac1a62b68c2a135be8a79ea1774e9f3cfcbe3
Ikrlegido12 <Ikermardi2010huawei@gmail.com>
2025-05-18 22:34:24 +0200

    Changing the template

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Bahia Guardias Web';
+export const SITE_DESCRIPTION = 'Sitio web de gestión de guardias.';
