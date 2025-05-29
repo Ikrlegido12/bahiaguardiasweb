9cb11c246b9758772fd8ebb876b5797cabf050f9
Ikrlegido12 <Ikermardi2010huawei@gmail.com>
2025-05-29 16:52:35 +0200

    Fix metadatos

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,4 @@
 export const SITE_TITLE = 'Bahia Guardias Web';
 export const SITE_DESCRIPTION = 'Sitio web de gestión de guardias. Creado por el IES BAHÍA DE ALGECIRAS';
 export const SITE_URL = 'https://bahiaguardiasweb.vercel.app/';
-export const SITE_IMAGE = '/images/logo.png';
+export const SITE_IMAGE = '/favicon.svg';
