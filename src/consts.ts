d22a48e997efe208aa92654dc4aa412a723b9971
Ikrlegido12 <Ikermardi2010huawei@gmail.com>
2025-05-29 16:52:02 +0200

    🖥 Nuevos metadatos

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Bahia Guardias Web';
-export const SITE_DESCRIPTION = 'Sitio web de gestión de guardias.';
+export const SITE_DESCRIPTION = 'Sitio web de gestión de guardias. Creado por el IES BAHÍA DE ALGECIRAS';
+export const SITE_URL = 'https://bahiaguardiasweb.vercel.app/';
+export const SITE_IMAGE = '/images/logo.png';
